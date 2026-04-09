339083508ac812301e889a3a3da6eb2c93614e46
Charly Mayeux <cmayeux@isagri.fr>
2026-04-09 07:32:54 +0000

    feat: implement home component and update routing for navigation

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,7 +2,16 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
     {
-        path: '',
+        path: 'home',
+        loadComponent: () => import("./home/home").then((m) => m.Home),
+    },
+    {
+        path: 'post',
         loadComponent: () => import("./posts.page/posts.page").then((m) => m.PostsPage),
+    },
+    {
+        path: '',
+        redirectTo: 'home',
+        pathMatch: 'full',
     }
 ];
