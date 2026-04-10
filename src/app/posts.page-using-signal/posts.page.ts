cf242597d304945ced9851a18d60bc48250ed3bd
Charly Mayeux <cmayeux@isagri.fr>
2026-04-10 11:34:16 +0000

    fix: correct post title typo and clean up comments in PostsPage component

--- src/app/posts.page-using-signal/posts.page.ts
+++ src/app/posts.page-using-signal/posts.page.ts
@@ -16,10 +16,7 @@ export class PostsPage {
   public readonly posts: Signal<Post[]> = this._postsService.posts;
   public readonly hasError: Signal<boolean> = this._postsService.hasError;
   public readonly error: Signal<ErrorResponse[]> = this._postsService.error;
-
-  // Le problème qu on a tjr eu avec angular :
-  // l'intégration de rxjs n a jamais été réussie, on a tjr eu besoin de faire du state management à la main pour gérer les sélections, les formulaires, etc... alors que c'est justement ce que rxjs est censé nous éviter de faire
-  // contrairement aux signals
+  
   private readonly _currentSelection: WritableSignal<Post>  = signal<Post>({
     id: '',
     title: '',
@@ -28,7 +25,6 @@ export class PostsPage {
   });
   public readonly currentSelection: FieldTree<PostResponse, string | number> = form<Post>(this._currentSelection);
   public onSelection(post: Post):void {
-    // shallow copie : on ne veut pas que les modifications faites dans le formulaire modifient directement l'objet de la liste, sinon on aurait des modifications en temps réel dans la liste au lieu d'avoir une validation à faire via un bouton submit
     this._currentSelection.set({...post});
   }
 
@@ -40,7 +36,6 @@ export class PostsPage {
       action: async (field) => {
         const selection = field().value();
         if (selection) {
-          // this._postsService.update(selection).pipe(take(1)).subscribe();
           await this._postsService.updateUsingPromise(selection);
           // l await ne sert à rien dans ce cas
         }
